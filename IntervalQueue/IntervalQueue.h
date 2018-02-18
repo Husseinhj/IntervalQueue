874927e1d66379db2dfb84ac0834aebf1c8d156f
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2018-02-18 10:43:46 +0330

    Initial Commit

---
+++ IntervalQueue/IntervalQueue.h
@@ -0,0 +1,19 @@
+//
+//  IntervalQueue.h
+//  IntervalQueue
+//
+//  Created by Hussein Habibi on 2/18/18.
+//  Copyright © 2018 Hussein.Juybari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IntervalQueue.
+FOUNDATION_EXPORT double IntervalQueueVersionNumber;
+
+//! Project version string for IntervalQueue.
+FOUNDATION_EXPORT const unsigned char IntervalQueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IntervalQueue/PublicHeader.h>
+
+
